9b692f147be122da1a57123b905db60a8c2b294b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:18:19 +0700

    connect user view model to user activity

--- app/src/main/java/com/example/androidretrofit/UserActivity.kt
+++ app/src/main/java/com/example/androidretrofit/UserActivity.kt
@@ -2,11 +2,22 @@ package com.example.androidretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 
 class UserActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        //pbUserProgressForm.visibility = View.VISIBLE
+        viewModel = ViewModelProviders.of(this)
+            .get(UserViewModel::class.java)
     }
 }
