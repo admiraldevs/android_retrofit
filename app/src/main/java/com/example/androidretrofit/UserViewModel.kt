1f0136bc2b35de4bccba70021dd00a13896c7ca7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:16:03 +0700

    call user repository in view model to get data

--- app/src/main/java/com/example/androidretrofit/UserViewModel.kt
+++ app/src/main/java/com/example/androidretrofit/UserViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.androidretrofit
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.androidretrofit.models.UserDataResponse
+import com.example.androidretrofit.networks.UserNetworkRepository
 
 class UserViewModel :ViewModel() {
-    
+
+    fun getListUsers(): MutableLiveData<ArrayList<UserDataResponse>> {
+        return UserNetworkRepository().getUsers()
+    }
 }
