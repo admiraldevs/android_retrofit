05e7e03080c451144a266459af0a53d3ba20f7bc
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 22:55:44 +0700

    add and setup for parceable model

--- app/src/main/java/com/example/androidretrofit/models/UserDataResponse.kt
+++ app/src/main/java/com/example/androidretrofit/models/UserDataResponse.kt
@@ -1,8 +1,12 @@
 package com.example.androidretrofit.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class UserDataResponse(
     val address: String,
     val id: Int,
     val name: String,
     val phone: String
-)
+): Parcelable
