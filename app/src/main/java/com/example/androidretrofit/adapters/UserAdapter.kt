c8704a3fccde9dfe02e591e5fca6897fd18cdb2b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:00:29 +0700

    implement user listener interface and setup for recycler adapter

--- app/src/main/java/com/example/androidretrofit/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/androidretrofit/adapters/UserAdapter.kt
@@ -1,5 +1,24 @@
 package com.example.androidretrofit.adapters
 
-class UserAdapter {
-    
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidretrofit.listeners.MyUserListener
+
+class UserAdapter(private val clickListener: MyUserListener) : RecyclerView.Adapter<UserAdapter.MyUserViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyUserViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: MyUserViewHolder, position: Int) {
+        
+    }
+
+    inner class MyUserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
 }
