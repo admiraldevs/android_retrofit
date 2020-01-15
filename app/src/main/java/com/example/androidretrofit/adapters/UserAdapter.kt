436b7ae0f45be04f379e5b959f067e1151d88e88
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:02:39 +0700

    setup for user adapter recycler

--- app/src/main/java/com/example/androidretrofit/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/androidretrofit/adapters/UserAdapter.kt
@@ -1,24 +1,41 @@
 package com.example.androidretrofit.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidretrofit.R
 import com.example.androidretrofit.listeners.MyUserListener
+import com.example.androidretrofit.models.UserDataResponse
+import kotlinx.android.synthetic.main.item_user.view.*
 
 class UserAdapter(private val clickListener: MyUserListener) : RecyclerView.Adapter<UserAdapter.MyUserViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyUserViewHolder {
 
+    private val myUsers = ArrayList<UserDataResponse>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyUserViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_user, parent, false)
+
+        return MyUserViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-
+        return myUsers.size
     }
 
     override fun onBindViewHolder(holder: MyUserViewHolder, position: Int) {
-        
+        holder.bind(myUsers[position])
     }
 
     inner class MyUserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        fun bind(myUser: UserDataResponse) {
+            itemView.tvMyUserName.text = myUser.name
+            itemView.tvMyUserAdress.text = myUser.address
+            itemView.tvMyUserMobile.text = myUser.phone
+            itemView.setOnClickListener {
+                clickListener.onClick(myUser)
+            }
+        }
     }
 }
