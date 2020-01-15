719de53726c8718687983c8771d903d1ba072732
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:51:17 +0700

    add user form activity

---
+++ app/src/main/java/com/example/androidretrofit/UserFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_form)
+    }
+}
