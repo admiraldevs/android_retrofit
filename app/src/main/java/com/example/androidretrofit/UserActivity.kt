b0e4bc5ed43da5dc21315f3585f0214082244e01
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-15 23:45:12 +0700

    set up user view model, set up user adapter, get data from api

--- app/src/main/java/com/example/androidretrofit/UserActivity.kt
+++ app/src/main/java/com/example/androidretrofit/UserActivity.kt
@@ -2,22 +2,71 @@ package com.example.androidretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.androidretrofit.adapters.UserAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class UserActivity : AppCompatActivity() {
 
+    private lateinit var adapter: UserAdapter
     private lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // set loading while getting data
+        showLoading(true)
+        // connect to user view model
         initViewModel()
+        // connect to user recycler adapter
+        initRecyclerView()
+        //get data from retrofit
+        fetchUserList()
     }
 
     private fun initViewModel() {
-        //pbUserProgressForm.visibility = View.VISIBLE
+        // initialize view model to user view model
         viewModel = ViewModelProviders.of(this)
             .get(UserViewModel::class.java)
+    }
+
+    private fun initRecyclerView() {
+        // setting user adapter for layout manager
+        rvUser.layoutManager = LinearLayoutManager(this)
+        // set up for user list divider
+        setupListDivider()
+        // initialize adapter to user adapter
+        adapter = UserAdapter()
+        rvUser.adapter = adapter
+    }
+
+    private fun setupListDivider() {
+        // set up for item list divider
+        val dividerItemDecoration = DividerItemDecoration(
+            rvUser.context, DividerItemDecoration.VERTICAL )
+        rvUser.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun fetchUserList() {
+        // get data from api
+        viewModel.getListUsers().observe(this, Observer {
+            // set data to user adapter
+            adapter.setMyUserList(it)
+
+            showLoading(false)
+        })
+    }
+
+    private fun showLoading(state: Boolean){
+        if(state){
+            pbUser.visibility = View.VISIBLE
+        } else {
+            pbUser.visibility = View.GONE
+        }
     }
 }
