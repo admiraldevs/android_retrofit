b14a3cab113a3012111c44ee0278ebcc332421c7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-17 22:17:09 +0700

    add function to get single data in user repository

--- app/src/main/java/com/example/androidretrofit/networks/UserNetworkRepository.kt
+++ app/src/main/java/com/example/androidretrofit/networks/UserNetworkRepository.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.androidretrofit.models.BaseUserResponse
 import com.example.androidretrofit.models.CreateUserResponse
+import com.example.androidretrofit.models.SingleUserResponse
 import com.example.androidretrofit.models.UserDataResponse
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,6 +12,7 @@ import retrofit2.Response
 
 class UserNetworkRepository {
 
+    // get all data
     fun getUsers(): MutableLiveData<ArrayList<UserDataResponse>> {
         val usersData = MutableLiveData<ArrayList<UserDataResponse>>()
         val listUsers = ArrayList<UserDataResponse>()
@@ -32,6 +34,7 @@ class UserNetworkRepository {
                         listUsers.add(response.body()?.data?.get(i) as UserDataResponse)
                     }
                     usersData.postValue(listUsers)
+                    Log.d("@@@Retrofit", "total: ${listUsers.size} items -- GET ALL")
                 }
             }
 
@@ -40,6 +43,38 @@ class UserNetworkRepository {
         return usersData
     }
 
+    // get single data
+    fun getUser(id : Int) : MutableLiveData<UserDataResponse> {
+
+        val userData = MutableLiveData<UserDataResponse>()
+
+        NetworkConfig.userApi().getUser(id).enqueue(object : Callback<SingleUserResponse<UserDataResponse>>{
+            override fun onFailure(call: Call<SingleUserResponse<UserDataResponse>>, t: Throwable) {
+                userData.postValue(null)
+            }
+
+            override fun onResponse(
+                call: Call<SingleUserResponse<UserDataResponse>>,
+                response: Response<SingleUserResponse<UserDataResponse>>
+            ) {
+
+                if(response.isSuccessful){
+                    val userResponse = response.body()?.data as UserDataResponse
+                    userData.postValue(userResponse)
+                    Log.d("@@@Retrofit", "get: ${userResponse.name}  -- GET ALL")
+                } else {
+                    userData.postValue(null)
+                }
+
+            }
+
+        })
+
+
+        return userData
+    }
+
+    // create data
     fun createUser(userData: UserDataResponse): MutableLiveData<UserDataResponse> {
         val createdUserData = MutableLiveData<UserDataResponse>()
 
