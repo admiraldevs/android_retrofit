fbd73bfe7d3d400608b4ede7e4f3f83674118d0b
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-17 22:18:36 +0700

    add function to get single data in user view model

--- app/src/main/java/com/example/androidretrofit/UserFormViewModel.kt
+++ app/src/main/java/com/example/androidretrofit/UserFormViewModel.kt
@@ -6,6 +6,11 @@ import com.example.androidretrofit.models.UserDataResponse
 import com.example.androidretrofit.networks.UserNetworkRepository
 
 class UserFormViewModel :ViewModel() {
+    // get single data
+    fun getUser(id: Int): MutableLiveData<UserDataResponse>{
+        return UserNetworkRepository().getUser(id)
+    }
+
     // create user
     fun createUser(userData: UserDataResponse)
             : MutableLiveData<UserDataResponse> {
