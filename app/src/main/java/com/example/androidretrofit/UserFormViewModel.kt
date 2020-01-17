776ea46f683d5dacc2b9732360d63a38bd2e1e15
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2020-01-17 21:05:17 +0700

    add function to create in view model

--- app/src/main/java/com/example/androidretrofit/UserFormViewModel.kt
+++ app/src/main/java/com/example/androidretrofit/UserFormViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.androidretrofit
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.androidretrofit.models.UserDataResponse
+import com.example.androidretrofit.networks.UserNetworkRepository
 
 class UserFormViewModel :ViewModel() {
-    
+    // create user
+    fun createUser(userData: UserDataResponse)
+            : MutableLiveData<UserDataResponse> {
+
+        return UserNetworkRepository().createUser(userData)
+    }
 }
